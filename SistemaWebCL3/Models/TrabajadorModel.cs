63f689f956e77f7ee333a1265a8364ccd1afa1e1
Lenovo <Lenovo@DESKTOP-JNL4T2L>
2023-08-20 18:21:31 -0500

    Agregar archivos de proyecto.

---
+++ SistemaWebCL3/Models/TrabajadorModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SistemaWebCL3.Models
+{
+    public class TrabajadorModel
+    {
+        public string dniTrabajador { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string empresa { get; set; }
+        public string ciudad { get; set; }
+        public string telefono { get; set; }
+        public string denuncia { get; set; }
+    }
+}
